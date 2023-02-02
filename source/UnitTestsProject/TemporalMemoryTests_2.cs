0c306f04e0903989225e9fa304b828677ead9627
Mousumi Parvin Tonny <mousumitonny@gmail.com>
2023-02-02 19:42:56 +0100

    My TemporalMemoryTest file for project
    I have created a file named TemporalMemoryTest_2 for our project  where I will write test case from myside.

---
+++ source/UnitTestsProject/TemporalMemoryTests_2.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Damir Dobric. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestsProject
+{
+    [TestClass]
+    public class TemporalPoolerTest2
+    {
+
+    }
+}
