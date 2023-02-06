8eacef178f181e4ca4c6e4f875fb5b2dc9dc9f0a
SadiaSabah <sadiasabah41@gmail.com>
2023-02-06 18:57:34 +0100

    variable setup done
    I have setup variable for further test

--- source/UnitTestsProject/TemporalMemoryTests -3.cs
+++ source/UnitTestsProject/TemporalMemoryTests -3.cs
@@ -14,5 +14,63 @@ namespace UnitTestsProject
     public class TemporalPoolerTest3
     {
 
+        private static bool areDisjoined<T>(ICollection<T> arr1, ICollection<T> arr2)
+        {
+            foreach (var item in arr1)
+            {
+                if (arr2.Contains(item))
+                    return false;
+            }
+
+            return true;
+        }
+        private Parameters getDefaultParameters()
+        {
+            Parameters retVal = Parameters.getTemporalDefaultParameters();
+            retVal.Set(KEY.COLUMN_DIMENSIONS, new int[] { 42 });
+            retVal.Set(KEY.CELLS_PER_COLUMN, 5);
+            retVal.Set(KEY.ACTIVATION_THRESHOLD, 3);
+            retVal.Set(KEY.INITIAL_PERMANENCE, 0.25);
+            retVal.Set(KEY.CONNECTED_PERMANENCE, 0.7);
+            retVal.Set(KEY.MIN_THRESHOLD, 2);
+            retVal.Set(KEY.MAX_NEW_SYNAPSE_COUNT, 4);
+            retVal.Set(KEY.PERMANENCE_INCREMENT, 0.20);
+            retVal.Set(KEY.PERMANENCE_DECREMENT, 0.20);
+            retVal.Set(KEY.PREDICTED_SEGMENT_DECREMENT, 0.0);
+            retVal.Set(KEY.RANDOM, new ThreadSafeRandom(42));
+            retVal.Set(KEY.SEED, 42);
+
+            return retVal;
+        }
+
+        private HtmConfig GetDefaultTMParameters()
+        {
+            HtmConfig htmConfig = new HtmConfig(new int[] { 42 }, new int[] { 42 })
+            {
+                CellsPerColumn = 5,
+                ActivationThreshold = 3,
+                InitialPermanence = 0.25,
+                ConnectedPermanence = 0.7,
+                MinThreshold = 2,
+                MaxNewSynapseCount = 4,
+                PermanenceIncrement = 0.2,
+                PermanenceDecrement = 0.2,
+                PredictedSegmentDecrement = 0,
+                Random = new ThreadSafeRandom(42),
+                RandomGenSeed = 42
+            };
+
+            return htmConfig;
+        }
+        private Parameters GetDefaultParameters1(Parameters p, string key, Object value)
+        {
+            Parameters retVal = p == null ? getDefaultParameters() : p;
+            retVal.Set(key, value);
+
+            return retVal;
+        }
+
+
     }
 }
+    
